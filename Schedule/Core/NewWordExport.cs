b5d67f14572361db1a2b92a240321c1c4d734f1c
Безуглов Сергей <Besuglov.S@Gmail.com>
2014-02-26 21:31:26 +0400

    Word Export +several sloc

--- Schedule/Core/NewWordExport.cs
+++ Schedule/Core/NewWordExport.cs
@@ -26,16 +26,77 @@ namespace Schedule.Core
             using (WordprocessingDocument wordDocument =
         WordprocessingDocument.Create(filename, WordprocessingDocumentType.Document))
             {
-                // Add a main document part. 
-                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
-
-                // Create the document structure and add some text.
-                mainPart.Document = new Document();
-                Body body = mainPart.Document.AppendChild(new Body());
-                Paragraph para = body.AppendChild(new Paragraph());
-                Run run = para.AppendChild(new Run());
-                run.AppendChild(new Text("Create text in body - CreateWordprocessingDocument"));
+                using (WordprocessingDocument package = WordprocessingDocument.Create(filePath, WordprocessingDocumentType.Document))
+                {
+                    CreateParts(package);
+                }
             }
+        }
+
+        private static void CreateParts(WordprocessingDocument document)
+        {
+            ExtendedFilePropertiesPart extendedFilePropertiesPart1 = document.AddNewPart<ExtendedFilePropertiesPart>("rId3");
+            GenerateExtendedFilePropertiesPart1Content(extendedFilePropertiesPart1);
+
+            MainDocumentPart mainDocumentPart1 = document.AddMainDocumentPart();
+            GenerateMainDocumentPart1Content(mainDocumentPart1);
+
+            WebSettingsPart webSettingsPart1 = mainDocumentPart1.AddNewPart<WebSettingsPart>("rId3");
+            GenerateWebSettingsPart1Content(webSettingsPart1);
+
+            DocumentSettingsPart documentSettingsPart1 = mainDocumentPart1.AddNewPart<DocumentSettingsPart>("rId2");
+            GenerateDocumentSettingsPart1Content(documentSettingsPart1);
+
+            StyleDefinitionsPart styleDefinitionsPart1 = mainDocumentPart1.AddNewPart<StyleDefinitionsPart>("rId1");
+            GenerateStyleDefinitionsPart1Content(styleDefinitionsPart1);
+
+            ThemePart themePart1 = mainDocumentPart1.AddNewPart<ThemePart>("rId5");
+            GenerateThemePart1Content(themePart1);
+
+            FontTablePart fontTablePart1 = mainDocumentPart1.AddNewPart<FontTablePart>("rId4");
+            GenerateFontTablePart1Content(fontTablePart1);
+
+            SetPackageProperties(document);
+        }
+
+        private static void SetPackageProperties(WordprocessingDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateFontTablePart1Content(FontTablePart fontTablePart1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateThemePart1Content(ThemePart themePart1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateStyleDefinitionsPart1Content(StyleDefinitionsPart styleDefinitionsPart1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateDocumentSettingsPart1Content(DocumentSettingsPart documentSettingsPart1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateWebSettingsPart1Content(WebSettingsPart webSettingsPart1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateMainDocumentPart1Content(MainDocumentPart mainDocumentPart1)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GenerateExtendedFilePropertiesPart1Content(ExtendedFilePropertiesPart extendedFilePropertiesPart1)
+        {
+            //throw new NotImplementedException();
         }
     }
 }
